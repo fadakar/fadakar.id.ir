b4447b74db204c0eee48da238abd73531e3e9577
gholamreza fadakar <fadakargholamreza@gmail.com>
2021-09-13 15:38:02 +0430

    add familiar skill and jquery smooth to element

--- src/app/modules/home/index/index.component.ts
+++ src/app/modules/home/index/index.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Skill} from "../_models/Skill";
+import * as $ from 'jquery';
+
 
 @Component({
   selector: 'app-home-index',
@@ -12,11 +14,12 @@ export class HomeIndexComponent implements OnInit {
     new Skill('PHP', 99),
     new Skill('طراحی', 70),
     new Skill('Laravel', 99),
+    new Skill('Lumen', 99),
     new Skill('MySql', 99),
     new Skill('Mongodb', 80),
     new Skill('Linux', 80),
     new Skill('Nginx', 80),
-    new Skill('Rest Api', 99),
+    new Skill('Rest', 99),
     new Skill('Html/Css', 99),
     new Skill('Bootstrap/Jquery', 90),
     new Skill('Javascript', 90),
@@ -29,9 +32,38 @@ export class HomeIndexComponent implements OnInit {
     new Skill('خلاقیت', 90),
     new Skill('روحیه تیمی', 90),
     new Skill('توانایی حل مسئله', 95),
+
+  ];
+
+  familiarSkills: Skill[] = [
+    new Skill('++C', 40),
+    new Skill('SASS', 40),
+    new Skill('Tailwind', 40),
+    new Skill('Express.js', 40),
+    new Skill('ASP.NET', 40),
+    new Skill('CodeIgniter', 40),
+    new Skill('Symfony', 40),
+    new Skill('Arduino', 40),
+    new Skill('Python', 40),
+    new Skill('JavaSE', 40),
+    new Skill('C#', 40),
+    new Skill('C#/Wpf', 40),
+    new Skill('C#/WindowsForm', 40),
+    new Skill('Docker', 40),
   ];
 
   constructor() { }
-  ngOnInit(): void {}
+  ngOnInit(): void {
 
+    $(window).on('load', function () {
+      $('.lds-ellipsis').fadeOut(); // will first fade out the loading animation
+      $('.preloader').delay(333).fadeOut('slow'); // will fade out the white DIV that covers the website.
+      $('body').delay(333);
+    });
+
+  }
+
+  scrollToElement($element: HTMLElement) {
+    $element.scrollIntoView({behavior: "smooth", block: "start", inline: "start"});
+  }
 }
