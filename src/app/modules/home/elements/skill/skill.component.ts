ce99b7e8dae4fa83ecb4557e75949e37243502aa
gholamreza fadakar <fadakargholamreza@gmail.com>
2021-09-13 15:36:54 +0430

    add custom color to skill component

--- src/app/modules/home/elements/skill/skill.component.ts
+++ src/app/modules/home/elements/skill/skill.component.ts
@@ -8,6 +8,8 @@ export class SkillComponent implements OnInit {
 
   @Input() name: string;
   @Input() percent: number;
+  @Input() color: 'bg-success' | 'bg-info' | 'bg-danger' | 'bg-warning' | 'bg-light' | 'bg-dark' = 'bg-success'
+  @Input() hexColor: string;
 
   constructor() { }
 
